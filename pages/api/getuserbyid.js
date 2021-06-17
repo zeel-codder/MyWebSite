068ea165701e2f8d941670a35cb1937ec9544b3e
zeel-codder <zeelprajapati321@gmail.com>
2021-06-17 17:42:01 +0530

    adding pythona and java coder

---
+++ pages/api/getuserbyid.js
@@ -0,0 +1,39 @@
+// const DataBase=require('server/database/DataBaseOperationsBlogInfo');
+const jwt = require('jsonwebtoken');
+
+import { ONConnections, OffConnections } from 'server/database/DataBaseConnection';
+import { GetOneUserInformationByEmail, GetOneUserInformationById } from 'server/database/user/CRUD'
+
+
+// import bcrypt from "bcryptjs";
+
+export default async function handler(req, res) {
+
+
+  // console.log(req.method)
+
+    const { id } = req.body;
+    // console.log(id)
+    // console.log(id);
+    await ONConnections();
+    const data = await GetOneUserInformationById(id);
+    await OffConnections();
+
+    if (data === null) {
+      // console.log('Lest Go');
+
+      res.status(404).end(JSON.stringify({
+        message: "User Not Found"
+      }));
+    }
+
+    res.end(JSON.stringify({
+      result: data
+    }));
+
+  
+
+
+
+
+};
