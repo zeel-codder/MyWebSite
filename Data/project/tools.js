ad70489afb391089432c35412e8488fea1879641
zeel-codder <zeelprajapati321@gmail.com>
2021-09-20 12:38:55 +0530

    Add

--- Data/project/tools.js
+++ Data/project/tools.js
@@ -6,8 +6,8 @@ const data=[
         title:'My Introduction',
         technology:['next js','Netlify','mongodb'],
         description:'My Personal Web site Build with the power of Next Js. App Has User Authentication, Nice Ui, Blogs on Trending Tech, Blog Like and Comment functionality.',
-        codelink:'https://github.com/zeel-codder/Game',
-        link:'https://zeelcodder.tech/',
+        codelink:'https://github.com/zeel-codder/MyWebSitNextjs',
+        link:'https://gun-master-game.netlify.app/',
         img:'/images/projects/my-blog.png'
     },
     {
@@ -16,8 +16,8 @@ const data=[
         title:'Gun master',
         technology:['TypeScript','Html5 Canvas','Node js','Socket-io'],
         description:'A Multiplayer Shotting Game Build in Html5 canvas and Node js. A hero can shoot the enemy by gun and play the game. Game support both guests as well as Multiplier Mode',
-        codelink:'https://github.com/zeel-codder/MyWebSitNextjs',
-        link:'https://gun-master-game.netlify.app/',
+        codelink:'https://github.com/zeel-codder/Game',
+        link:'https://zeelcodder.tech/',
         
     },
 
