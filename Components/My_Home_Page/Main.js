69c75a715ed4fcb9b286940ed482342a1dfe99ff
zeel-codder <zeelprajapati321@gmail.com>
2022-04-14 13:16:12 +0530

    update link

--- Components/My_Home_Page/Main.js
+++ Components/My_Home_Page/Main.js
@@ -9,7 +9,6 @@
  * @functionality : This Component is Main page of zeel-codder web site. which Combine the      Introduction,Education,Project and Technology Info
  */
 
-
 // Imports
 // ====================================
 // import { useGlobalContext } from '../context';
@@ -17,65 +16,48 @@
 // import { useState } from 'react';
 // import {Link} from 'react-router-dom';
 // import { useHistory } from "react-router-dom";
-import Introduction from '@Home/introduction';
-import Education from './education';
-import Technology from './tecnologes';
-import Projects from './projects';
-import Contact from './contect';
+import Introduction from "@Home/introduction";
+import Education from "./education";
+import Technology from "./tecnologes";
+import Projects from "./projects";
+import Contact from "./contect";
 import React from "react";
 
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 // ====================================
 
-
 function Main() {
-
-
   return (
-      <>
-        <div className="main1" id="home">
-         
-            <img src='/images/home/zeel.jpeg' className="logo" alt="Zeel"></img>
-            <div className="text-box-one">
-            <Fade top>
-
+    <>
+      <div className="main1" id="home">
+        <img src="/images/home/zeel.jpeg" className="logo" alt="Zeel"></img>
+        <div className="text-box-one">
+          <Fade top>
             <span className="logo-text" id="head">
-
-             Hi 👋, I am zeel
-
-            
-             
+              Hi 👋, I am zeel
             </span>
-            </Fade>
-            <Fade bottom>
-
-            <span className="logo-texth">
-
-              I am Developer
-            </span>
+          </Fade>
+          <Fade bottom>
+            <span className="logo-texth">I am Developer</span>
 
             <button className="btn">
-            <a href="https://drive.google.com/file/d/1mkCDqYevcKf8cuSH2ChvB-sjcVbyydAR/view?usp=sharing">
-                
+              <a href="https://drive.google.com/file/d/1NQCxkmtmI-j2Zm71px-UIus5lKq44WvI/view?usp=sharing">
                 Resume
-                </a>
+              </a>
             </button>
-            
-            </Fade>
-            </div>
-         
+          </Fade>
         </div>
+      </div>
 
-        {/*  Main component's */}
-        <section className="container mybox">
-          
-          <Introduction id="introduction"/>
-          {/* <Education /> */}
-          <Technology />
-          <Projects />
-          <Contact></Contact>
-        </section>
-        </>
+      {/*  Main component's */}
+      <section className="container mybox">
+        <Introduction id="introduction" />
+        {/* <Education /> */}
+        <Technology />
+        <Projects />
+        <Contact></Contact>
+      </section>
+    </>
   );
 }
 
